1287b40d3fb141a695587887e6dfd35f7ae8c039
EliaSantapaga <111609381+EliaSantapaga@users.noreply.github.com>
2024-03-25 19:10:58 +0100

    Fix

--- src/components/CharacterInfo.jsx
+++ src/components/CharacterInfo.jsx
@@ -1,41 +1,44 @@
 /* eslint-disable react/no-unescaped-entities */
-import pyro from "../assets/Elements/Pyro.png";
-import amber from "../assets/Characters/Amber.png";
-import vaBefore from "../assets/Other/VA-before.jpg";
+import pyro from '../assets/Elements/Pyro.png';
+import amber from '../assets/Characters/Amber.png';
+import vaBefore from '../assets/Other/VA-before.jpg';
 
 function CharacterInfo() {
   return (
-    <div className="character-slide row">
-      <div className="info-div col-5 pt-5">
+    <div className='character-slide row'>
+      <div className='info-div col-5 pt-5'>
         <img
           src={pyro}
-          alt="Pyro Logo"
-          className="element-logo opacity-25 position-absolute"
+          alt='Pyro Logo'
+          className='element-logo opacity-25 position-absolute'
         />
-        <div className="align-items-center">
-          <h1 className="character-name noto-serif text-white">AMBER</h1>
+        <div className='align-items-center'>
+          <h1 className='character-name noto-serif text-white'>AMBER</h1>
 
-          <div className="va-info-row row mb-4">
-            <div className="col-9 va-info d-flex p-0">
-              <div className="col-2 d-flex align-items-center">
-                <img src={vaBefore} alt="VA before" />
+          <div className='va-info-row row mb-4'>
+            <div className='col-9 va-info d-flex p-0'>
+              <div className='col-2 d-flex align-items-center'>
+                <img
+                  src={vaBefore}
+                  alt='VA before'
+                />
               </div>
-              <div className="col-10 d-flex align-items-center">
-                <p className="va-name fs-3 m-0">VA: Manaka Iwami</p>
+              <div className='col-10 d-flex align-items-center'>
+                <p className='va-name fs-3 m-0'>VA: Manaka Iwami</p>
               </div>
             </div>
 
-            <div className="col-3 center-flex">
-              <label class="switch">
-                <input type="checkbox" />
-                <span class="slider round" />
+            <div className='col-3 center-flex'>
+              <label className='switch'>
+                <input type='checkbox' />
+                <span className='slider round' />
               </label>
             </div>
           </div>
 
-          <div className="infobox ps-3 py-2 d-flex">
-            <div className="infobox-inside">
-              <p className="info-text text-white p-0 m-0">
+          <div className='infobox ps-3 py-2 d-flex'>
+            <div className='infobox-inside'>
+              <p className='info-text text-white p-0 m-0'>
                 Una ragazza vivace e diretta, nonché la Vigilante dei Cavalieri
                 di Favonius.
                 <br />
@@ -50,8 +53,12 @@ function CharacterInfo() {
         </div>
       </div>
 
-      <div className="col-7 d-flex justify-content-end">
-        <img src={amber} alt="Amber" className="character-fullart" />
+      <div className='col-7 d-flex justify-content-end'>
+        <img
+          src={amber}
+          alt='Amber'
+          className='character-fullart'
+        />
       </div>
     </div>
   );
