237bc4cf07b2756c2bf993902521422594c6d572
EliaSantapaga <111609381+EliaSantapaga@users.noreply.github.com>
2024-03-26 11:57:32 +0100

    Toggle funzionante

--- src/components/CharacterInfo.jsx
+++ src/components/CharacterInfo.jsx
@@ -1,44 +1,52 @@
 /* eslint-disable react/no-unescaped-entities */
-import pyro from '../assets/Elements/Pyro.png';
-import amber from '../assets/Characters/Amber.png';
-import vaBefore from '../assets/Other/VA-before.jpg';
+import pyro from "../assets/Elements/Pyro.png";
+import amber from "../assets/Characters/Amber.png";
+import vaBefore from "../assets/Other/VA-before.jpg";
+import { useState } from "react";
 
 function CharacterInfo() {
+  const [check, setCheck] = useState(false);
+
+  const handleToggleChange = (event) => {
+    setCheck(event.target.checked);
+  };
+
   return (
-    <div className='character-slide row'>
-      <div className='info-div col-5 pt-5'>
+    <div className="character-slide row">
+      <div className="info-div col-5 pt-5">
         <img
           src={pyro}
-          alt='Pyro Logo'
-          className='element-logo opacity-25 position-absolute'
+          alt="Pyro Logo"
+          className="element-logo opacity-25 position-absolute"
         />
-        <div className='align-items-center'>
-          <h1 className='character-name noto-serif text-white'>AMBER</h1>
+        <div className="align-items-center">
+          <h1 className="character-name noto-serif text-white">AMBER</h1>
 
-          <div className='va-info-row row mb-4'>
-            <div className='col-9 va-info d-flex p-0'>
-              <div className='col-2 d-flex align-items-center'>
-                <img
-                  src={vaBefore}
-                  alt='VA before'
-                />
+          <div className="va-info-row row mb-4">
+            <div className="col-9 va-info d-flex p-0">
+              <div className="col-2 d-flex align-items-center">
+                <img src={vaBefore} alt="VA before" />
               </div>
-              <div className='col-10 d-flex align-items-center'>
-                <p className='va-name fs-3 m-0'>VA: Manaka Iwami</p>
+              <div className="col-10 d-flex align-items-center">
+                {check ? (
+                  <p className="va-name fs-3 m-0">VA: Kelly Baskin</p>
+                ) : (
+                  <p className="va-name fs-3 m-0">VA: Manaka Iwami</p>
+                )}
               </div>
             </div>
 
-            <div className='col-3 center-flex'>
-              <label className='switch'>
-                <input type='checkbox' />
-                <span className='slider round' />
+            <div className="col-3 center-flex">
+              <label class="switch">
+                <input type="checkbox" onChange={handleToggleChange} />
+                <span class="slider round" />
               </label>
             </div>
           </div>
 
-          <div className='infobox ps-3 py-2 d-flex'>
-            <div className='infobox-inside'>
-              <p className='info-text text-white p-0 m-0'>
+          <div className="infobox ps-3 py-2 d-flex">
+            <div className="infobox-inside">
+              <p className="info-text text-white p-0 m-0">
                 Una ragazza vivace e diretta, nonché la Vigilante dei Cavalieri
                 di Favonius.
                 <br />
@@ -53,12 +61,8 @@ function CharacterInfo() {
         </div>
       </div>
 
-      <div className='col-7 d-flex justify-content-end'>
-        <img
-          src={amber}
-          alt='Amber'
-          className='character-fullart'
-        />
+      <div className="col-7 d-flex justify-content-end">
+        <img src={amber} alt="Amber" className="character-fullart" />
       </div>
     </div>
   );
