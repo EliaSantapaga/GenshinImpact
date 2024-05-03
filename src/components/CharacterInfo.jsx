6fbd7603b33624f7bdbf17562efbcc60271e764c
EliaSantapaga <111609381+EliaSantapaga@users.noreply.github.com>
2024-05-03 14:48:11 +0200

    Modifiche Swiper React

--- src/components/CharacterInfo.jsx
+++ src/components/CharacterInfo.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from 'react';
+import { useState } from "react";
 // import { useParams } from 'react-router-dom';
-import { useCharacterContext } from '../context/CharacterContext';
-import pyro from '../assets/elements/Pyro.png';
-import amber from '../assets/characters/Amber.png';
-import vaBefore from '../assets/other/VA-before.jpg';
+import { useCharacterContext } from "../context/CharacterContext";
+import pyro from "../assets/elements/Pyro.png";
+import amber from "../assets/characters/Amber.png";
+import vaBefore from "../assets/other/VA-before.jpg";
 // import AppContext from "../context/AppContext";
 
 function CharacterInfo() {
@@ -25,50 +25,44 @@ function CharacterInfo() {
   };
 
   return (
-    <div className='character-slide row'>
-      <div className='info-div col-5 pt-5'>
+    <div className="character-slide row">
+      <div className="info-div col-5 pt-5">
         <img
           src={pyro}
-          alt='Pyro Logo'
-          className='element-logo opacity-25 position-absolute'
+          alt="Pyro Logo"
+          className="element-logo opacity-25 position-absolute"
         />
-        <div className='align-items-center'>
-          <h1 className='character-name noto-serif text-white m-0'>
+        <div className="align-items-center">
+          <h1 className="character-name noto-serif text-white m-0">
             AMBER
             {/* {selectedCharacter.name} */}
           </h1>
 
-          <div className='va-info-row row mb-4'>
-            <div className='col-9 va-info d-flex p-0'>
-              <div className='col-2 d-flex align-items-center'>
-                <img
-                  src={vaBefore}
-                  alt='VA before'
-                />
+          <div className="va-info-row row mb-4">
+            <div className="col-9 va-info d-flex p-0">
+              <div className="col-2 d-flex align-items-center">
+                <img src={vaBefore} alt="VA before" />
               </div>
-              <div className='col-10 d-flex align-items-center'>
+              <div className="col-10 d-flex align-items-center">
                 {check ? (
-                  <p className='va-name fs-3 m-0'>VA: Kelly Baskin</p>
+                  <p className="va-name fs-3 m-0">VA: Kelly Baskin</p>
                 ) : (
-                  <p className='va-name fs-3 m-0'>VA: Manaka Iwami</p>
+                  <p className="va-name fs-3 m-0">VA: Manaka Iwami</p>
                 )}
               </div>
             </div>
 
-            <div className='col-3 center-flex'>
-              <label className='switch'>
-                <input
-                  type='checkbox'
-                  onChange={handleToggleChange}
-                />
-                <span className='slider round' />
+            <div className="col-3 center-flex">
+              <label className="switch">
+                <input type="checkbox" onChange={handleToggleChange} />
+                <span className="slider round" />
               </label>
             </div>
           </div>
 
-          <div className='infobox ps-3 py-2 d-flex'>
-            <div className='infobox-inside'>
-              <p className='info-text text-white p-0 m-0'>
+          <div className="infobox ps-3 py-2 d-flex">
+            <div className="infobox-inside">
+              <p className="info-text text-white p-0 m-0">
                 Una ragazza vivace e diretta, nonché la Vigilante dei Cavalieri
                 di Favonius.
                 <br />
@@ -83,12 +77,8 @@ function CharacterInfo() {
         </div>
       </div>
 
-      <div className='col-7 d-flex justify-content-end'>
-        <img
-          src={amber}
-          alt='Amber'
-          className='character-fullart'
-        />
+      <div className="col-7 d-flex justify-content-end">
+        <img src={amber} alt="Amber" className="character-fullart" />
       </div>
     </div>
   );
--- src/components/CharacterSlider.jsx
+++ src/components/CharacterSlider.jsx
@@ -1,7 +1,18 @@
-import leftButton from '../assets/other/Left-button.png';
-import rightButton from '../assets/other/Right-button.png';
-import { useCharacterContext } from '../context/CharacterContext';
+import { useRef, useState } from "react";
+import leftButton from "../assets/other/Left-button.png";
+import rightButton from "../assets/other/Right-button.png";
+import { useCharacterContext } from "../context/CharacterContext";
 // import CharacterCard from './CharacterCard';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Navigation } from "swiper/modules";
 
 function CharacterSlider() {
   const { characterData } = useCharacterContext();
@@ -10,37 +21,67 @@ function CharacterSlider() {
   console.log(characterData);
 
   return (
-    <div className='character-slider d-flex container-fluid pb-3'>
-      <div className='col-3 d-flex justify-content-end'>
-        <button className='slider-button'>
+    <div className="character-slider d-flex container-fluid pb-3">
+      <div className="col-3 d-flex justify-content-end pe-3">
+        <button className="slider-button">
           <img
             src={leftButton}
-            alt='Previous'
+            alt="Previous"
+            class="swiper-button-prev"
+            tabindex="-1"
+            role="button"
+            aria-label="Previous Character"
+            aria-controls="swiper-wrapper-8ac752d74ef73fc7"
           />
         </button>
       </div>
-      <div className='col-6 d-flex align-items-center mx-0'>
-        {characterData.map((char) => (
-          <>
-            <div className='col-2'>
+      <div className="col-6 ">
+        <Swiper
+          slidesPerView={6}
+          spaceBetween={30}
+          loop={true}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper d-flex align-items-center"
+        >
+          {characterData.map((char) => (
+            <>
+              <SwiperSlide key={char.id}>
+                <div className="rounded border-card">
+                  <div className="character-card rounded center-flex">
+                    <img src={char.avatar} alt={char.name} />
+                    <p className="character-card-name fs-6 text-center py-2">
+                      {char.name}
+                    </p>
+                  </div>
+                </div>
+              </SwiperSlide>
+
+              {/* <div className='col-2'>
               <div className='character-card rounded center-flex'>
-                <img
-                  src={char.avatar}
-                  alt={char.name}
-                />
-                <p className='character-card-name fs-6 text-center py-2'>
-                  {char.name}
-                </p>
+              <img
+              src={char.avatar}
+              alt={char.name}
+              />
+              <p className='character-card-name fs-6 text-center py-2'>
+              {char.name}
+              </p>
               </div>
-            </div>
-          </>
-        ))}
+            </div> */}
+            </>
+          ))}
+        </Swiper>
       </div>
-      <div className='col-3 d-flex justify-content-start'>
-        <button className='slider-button'>
+      <div className="col-3 d-flex justify-content-start ps-3">
+        <button className="slider-button">
           <img
             src={rightButton}
-            alt='Next'
+            alt="Next"
+            class="swiper-button-next"
+            tabindex="+1"
+            role="button"
+            aria-label="Next character"
+            aria-controls="swiper-wrapper-8ac752d74ef73fc7"
           />
         </button>
       </div>
